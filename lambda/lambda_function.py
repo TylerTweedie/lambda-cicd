d7c664b7cf1e967970e029b5f37436635369d602
Tyler Tweedie <tylertweedie@Tylers-MacBook-Air.local>
2025-08-05 22:05:37 -0400

    updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,5 +2,5 @@ import json
 def lambda_handler (event, context):
     return {
         'statuscode': 200,
-        'body': json.dumps('Hello from updated Lambda from vscode')
+        'body': json.dumps('Hello from updated Lambda from vscode!')
     }
